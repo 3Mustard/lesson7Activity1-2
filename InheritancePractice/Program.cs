d9f4e64bb4bd0949a67491ec779e92304c54a4f6
Matthew Cullen <m23.cullen@gmail.com>
2021-01-27 19:33:17 -0500

    solved problem 1

---
+++ InheritancePractice/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Problem1
+{
+    public class Animal
+    {
+        public virtual string GiveBirth()
+        {
+            return "I don't know how";
+        }
+    }
+
+    public class Mammal : Animal
+    {
+        public override string GiveBirth()
+        {
+            return "I give live birth";
+        }
+    }
+
+    public class Bird : Animal
+    {
+        public override string GiveBirth()
+        {
+            return "I lay eggs";
+        }
+    }
+}
